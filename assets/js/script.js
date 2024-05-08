3eb8d96842c5543cbd8830c4a3cb311ca3502e6c
Marwen Ben Ibrahim <marwenbrahim12@gmail.com>
2024-05-08 12:46:27 +0100

    last changes before presenting

--- assets/js/script.js
+++ assets/js/script.js
@@ -108,3 +108,14 @@ document.addEventListener("DOMContentLoaded", function() {
     notSection.classList.remove("active");
   });
 });
+document.getElementById("signupForm").addEventListener("submit", function(event) {
+  // Prevent default form submission
+  event.preventDefault();
+  // Redirect to formulaire.html
+  window.location.href = "formulaire.html";
+});
+
+document.getElementById("closeButton").addEventListener("click", function() {
+  // Redirect to formulaire.html when close button is clicked
+  window.location.href = "formulaire.html";
+});
